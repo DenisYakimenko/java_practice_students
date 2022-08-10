7ba39740df6ca0c65af861e645ebeca04c1bc71b
DenisYakimenko <dyakimenko@mail.ru>
2022-08-10 22:51:30 +0300

    add class Person

---
+++ src/edu/javacourse/studentorder/domain/Person.java
@@ -0,0 +1,17 @@
+package edu.javacourse.studentorder.domain;
+
+import java.time.LocalDate;
+
+public class Person
+{
+    private String surName;
+    private String givenName;
+    private String patronymic;
+    private LocalDate dateOfBirth;
+
+    private String postCode;
+    private String street;
+    private String extension;
+    private String apartment;
+
+}
