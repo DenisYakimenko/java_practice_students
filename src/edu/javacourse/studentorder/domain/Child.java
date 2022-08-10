3446ac0b0a9a1b3ff74f156d8424a75babd5f7f8
DenisYakimenko <dyakimenko@mail.ru>
2022-08-10 23:14:44 +0300

    add class Child

---
+++ src/edu/javacourse/studentorder/domain/Child.java
@@ -0,0 +1,10 @@
+package edu.javacourse.studentorder.domain;
+
+import java.time.LocalDate;
+
+public class Child
+{
+    private String certificateNumber;
+    private LocalDate issueDate;
+    private String issueDepartment;
+}
